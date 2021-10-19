0730c5e2d89e00b4ceb49a2c115241c4fe1f2180
kchelaki <konstantinos.chelakis@cern.ch>
2021-10-19 17:41:59 +0200

    add bagit validate and json schema validate - JSON Schema needs to be updated to the new sip.json standard

--- oais_utils/validate.py
+++ oais_utils/validate.py
@@ -1,17 +1,8 @@
 import logging
 import json
 import os
-
-# Extract given field from json dictonary
-def get_field_from_json(data, field):
-    try:
-        if not data[field]:
-            raise Exception(f"Required 'bic-meta.json' field: {field} is empty.")
-        else:
-            logging.info(f"\tFound field: {field}")
-        return data[field]
-    except KeyError:
-        raise Exception(f"'bic-meta.json' does not contain a required field: {field}")
+import bagit
+from jsonschema import validate
 
 
 # Verify if folder exists
@@ -21,7 +12,19 @@ def verify_folder_exists(path):
         raise Exception(f"Directory {path} does not exist.")
 
 
-# Verify whether AIU and AIC directories exists
+# Verify bag
+def verify_bag(path):
+    bag = bagit.Bag(path)
+    valid = False
+    try:
+        valid = bag.validate()
+    except bagit.BagValidationError as err:
+        print(f"Bag validation failed: {err}")
+    if not valid:
+        raise Exception(f"Bag validation error")
+
+
+# Verify whether AIU and SIP directories exists
 def verify_directory_structure(path):
     logging.info("Verifying directory structure")
 
@@ -29,19 +32,7 @@ def verify_directory_structure(path):
     if not os.path.exists(data_path):
         raise Exception("Directory named 'data' does not exist.")
 
-    aic_folder = None
-    bagit_txt = False
-    baginfo_txt = False
-
-    for dir in os.listdir(path):
-
-        if dir == "bagit.txt":
-            logging.info("Verifying bagit.txt file..")
-            bagit_txt = True
-
-        if dir == "bag-info.txt":
-            logging.info("Verifying bag-info.txt file..")
-            baginfo_txt = True
+    sip_folder = None
 
     for dir in os.listdir(data_path):
         logging.info("Verifying data/content folder..")
@@ -55,43 +46,41 @@ def verify_directory_structure(path):
             for sub in os.listdir(dir_path):
                 empty = False
                 if "sip.json" in sub:
-                    is_aic = True
+                    is_sip = True
 
             if empty:
                 raise Exception(f"Empty directory found: {dir_path}")
 
-            if is_aic:
-                aic_folder = dir_path
-                logging.info(f"\tFound AIC file directory: {dir_path}")
+            if is_sip:
+                sip_folder = dir_path
+                logging.info(f"\tFound SIP file directory: {sip_folder}")
             else:
                 logging.info(f"\tFound AIU file directory: {dir_path}")
 
-    if not bagit_txt:
-        raise Exception("bagit.txt was not found.")
-    if not baginfo_txt:
-        raise Exception("bag-info.txt was not found.")
-    if not aic_folder:
-        raise Exception("AIC directory was not found.")
+    if not sip_folder:
+        raise Exception("sip directory was not found.")
 
-    return aic_folder
+    return sip_folder
 
 
-# Check whether bic-meta.json contains the required fields
-def validate_bic_meta(path, aic_folder):
+# Check whether sip.json contains the required fields
+def validate_sip(path, sip_folder):
     logging.info("Validating sip.json")
     try:
-        with open(os.path.join(aic_folder, "sip.json")) as json_file:
+
+        with open(os.path.join(sip_folder, "sip.json")) as json_file:
             data = json.load(json_file)
 
-            required_fields = ["metadataFile_upstream", "contentFiles"]
-            for field in required_fields:
-                get_field_from_json(data, field)
+        with open("sip-schema-d1.json") as json_schema:
+            schema = json.load(json_schema)
 
-    except FileNotFoundError:
-        raise Exception("bic-meta.json file not found.")
+        validate(instance=data, schema=schema)
+
+    except Exception as err:
+        print(err)
 
 
-# Validate data according to the CERN AIP specification
+# Validate data according to SIP specification
 def validate_aip(path):
     logging.basicConfig(level=20, format="%(message)s")
     logging.info("Starting validation")
@@ -99,9 +88,11 @@ def validate_aip(path):
     try:
         verify_folder_exists(path)
 
-        aic_folder = verify_directory_structure(path)
+        verify_bag(path)
 
-        validate_bic_meta(path, aic_folder)
+        sip_folder = verify_directory_structure(path)
+
+        validate_sip(path, sip_folder)
 
         logging.info("Validation ended successfully.")
 
