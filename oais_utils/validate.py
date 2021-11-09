7dea4735fece9b227b577f5c49f4e8a2d9fec075
kchelaki <konstantinos.chelakis@cern.ch>
2021-11-09 18:00:30 +0100

    added content validate and list as an input to validate directory structure

--- oais_utils/validate.py
+++ oais_utils/validate.py
@@ -3,7 +3,7 @@ import json
 import os
 import bagit
 from jsonschema import validate
-from schemas import draft
+import schemas
 
 
 # Verify if folder exists
@@ -25,41 +25,64 @@ def verify_bag(path):
         raise Exception(f"Bag validation error")
 
 
-# Verify whether SIP directory exists
-def verify_directory_structure(path):
+def verify_directory_structure(path, dirlist):
     logging.info("Verifying directory structure")
-
-    data_path = os.path.join(path, "data")
-    content_path = os.path.join(data_path, "content")
-    meta_path = os.path.join(data_path, "meta")
-
-    paths = [data_path, content_path, meta_path]
-
-    for path in paths:
-        logging.info(f"Checking if {path} exists")
-        if not os.path.exists(path):
-            raise Exception(f"Directory {path} does not exist.")
+    for directory in dirlist:
+        dir_path = os.path.join(path, directory)
+        logging.info(f"Checking if {dir_path} exists")
+        if not os.path.exists(dir_path):
+            raise Exception(f"Directory {dir_path} does not exist.")
         else:
             logging.info(f"\tSuccessful")
 
-    return meta_path
-
 
 # Check whether sip.json contains the required fields
-def validate_sip(sip_folder, schema):
+def validate_sip(path, schema, sip_json_name="sip.json"):
     logging.info("Validating sip.json")
     try:
+        for root, dirs, files in os.walk(path):
+            if sip_json_name in files:
+                sip_file = os.path.join(root, sip_json_name)
+                with open(sip_file) as json_file:
+                    data = json.load(json_file)
 
-        with open(os.path.join(sip_folder, "sip.json")) as json_file:
-            data = json.load(json_file)
+                # Get the json as a from draft function.
+                check_schema = schemas.draft(schema)
 
-        # Get draft0 from draft function.
-        schema = draft()
+                validate(instance=data, schema=check_schema)
 
-        validate(instance=data, schema=schema)
+                logging.info(f"\tValidated successfully against the {schema}")
+                return sip_file
 
-    except Exception as err:
-        print(err)
+    except Exception:
+        raise Exception(f"SIP Validation failed")
+
+
+# Check if the content folder exists and contains all the files
+def validate_contents(path, sip_file):
+    logging.info("Validate contents folder")
+
+    with open(sip_file) as json_file:
+        data = json.load(json_file)
+
+    is_dry = data["audit"][0]["tool"]["params"]["dry_run"]
+
+    if not is_dry:
+        try:
+            content_files = data["contentFiles"]
+            for file in content_files:
+                bagpath = file["bagpath"]
+                downloaded = file["downloaded"]
+                if downloaded:
+                    full_bagpath = os.path.join(path, bagpath)
+                    if os.path.exists(full_bagpath):
+                        logging.info(f"\tFile in path: {bagpath} exists")
+                    else:
+                        raise Exception(f"File in path: {bagpath} does not exist")
+        except:
+            raise Exception("Error with the contentFiles")
+    else:
+        logging.info(f"\tThis is a dry_run. No contents to validate")
 
 
 # Validate data according to SIP specification
@@ -70,11 +93,13 @@ def validate_sip_folder(path, schema="draft1"):
     try:
         verify_folder_exists(path)
 
+        verify_directory_structure(path, dirlist=["data", "data/content", "data/meta"])
+
+        sip_file = validate_sip(path, schema)
+
+        validate_contents(path, sip_file)
+
         verify_bag(path)
-
-        sip_folder = verify_directory_structure(path)
-
-        validate_sip(sip_folder, schema)
 
         logging.info("Validation ended successfully.")
 
