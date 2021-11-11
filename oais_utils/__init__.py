29fc467911b23f8f344830973a8f6e5e8ebf915d
Antonio Vivace <avivace4@gmail.com>
2021-11-11 14:47:53 +0100

    Restructure schemas reading

--- oais_utils/__init__.py
+++ oais_utils/__init__.py
@@ -1,2 +1,14 @@
-import oais_utils.validate
-import oais_utils.schemas
+# import oais_utils.validate
+
+json_schemas_paths = {"draft1": "oais_utils/schemas/sip-schema-d1.json"}
+
+import json
+
+
+def schemas():
+    json_schemas = {}
+    for schema_name in json_schemas_paths:
+        with open(json_schemas_paths[schema_name]) as f:
+            json_schemas[schema_name] = json.load(f)
+
+    return json_schemas
